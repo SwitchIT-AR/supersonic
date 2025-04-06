ecd0138761368e9ade4e08a2cccd459c77505659
matieckhardt <matias.eckhardt@gmail.com>
2025-04-06 16:48:01 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import viteLogo from '/supersonic.png'
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <div>
+                <img src={viteLogo} className="logo" alt="Vite logo" />
+            </div>
+            <h1>
+                Más allá de lo conocido existe un universo de oportunidades infinitas
+            </h1>
+            <p>Muy pronto vas a ver lo nuevo de <strong>Supersonic</strong>,
+                nos estamos renovando. <br/>
+                Mientras tanto, podés comunicarte con nosotros a{' '} <br/>
+                <i className="ti ti-mail"></i>{' '}
+                <a style={{textDecoration: "white"}} href="mailto:info@supersonic.com.ar">info@supersonic.com.ar</a> <br />
+                <i className="ti ti-brand-whatsapp"></i>
+                 <a style={{textDecoration: "white"}} href="mailto:info@supersonic.com.ar"> +54 9 11 5859 8061
+                 </a>
+
+            </p>
+        </>
+    )
+}
+
+115 - 859 - 8061
+export default App
